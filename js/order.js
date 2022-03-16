aceab3891229f772f1a169d4b795ff393db3f6a3
Mario Salazar <salazar10mario.1995@gmail.com>
2022-03-16 15:56:18 -0600

    first commit by Mario Salazar

---
+++ js/order.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '46499',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '31516',
+        paymentStatus: 'Rufunded',
+        shipping:'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Done',
+        productNumber: '5265',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '3648',
+        paymentStatus: 'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '46499',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    }
+]
